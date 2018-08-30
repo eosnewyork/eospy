268305735a076ef98d9569afc49c15d76c35336d
William Deck <deck@eosnewyork.io>
2018-08-30 14:47:08 -0400

    v1.1.3 remove circular dependency with buffer/types fix imports for windows/linux

--- eospy/types.py
+++ eospy/types.py
@@ -1,10 +1,18 @@
-from .buffer import EOSBuffer 
+
 from .schema import ActionSchema, PermissionLevelSchema, ChainInfoSchema, BlockInfoSchema, TransactionSchema
-#import utils
 import datetime as dt
 import pytz
-from .utils import sha256
+from .utils import sha256, string_to_name, int_to_hex
 import json
+import binascii
+
+def convert_little_endian(buf, format='q') :
+    ''' '''
+    return struct.pack('<{}'.format(format), buf)
+
+def convert_big_endian(buf) :
+    ''' '''
+    return struct.pack('>{}'.format(format), buf)
 
 # json encoder
 class EOSEncoder(json.JSONEncoder) :
@@ -52,7 +60,7 @@ class VarUInt :
             self._push_byte(buf)
         return self._b_arr
 
-# 
+#
 class BaseObject(object) :
     def __init__(self, d) :
         ''' '''
@@ -193,3 +201,67 @@ class Transaction(BaseObject) :
         
     def get_id(self) :
         return sha256(self.encode())
+
+class EOSBuffer :
+    def __init__(self, v) :
+        self._value = v
+        self._count = 0
+    
+    def _write_number(self, val, format='q') :
+        ''' '''
+        le = convert_little_endian(val, format)
+        return binascii.hexlify(le)
+
+    def _write_name(self, w_str) :
+        ''' '''
+        val = string_to_name(w_str)
+        le = convert_little_endian(val, 'Q')
+        return binascii.hexlify(le)
+
+    def _write_str(self, w_str) :
+        return binascii.hexlify(chr(len(w_str)) +w_str)
+
+    def _write_varuint(self, vuint) :
+        buf = vuint.encode()
+        return binascii.hexlify(buf)
+        
+    def encode(self, val=None) :
+        if not val :
+            val = self._value
+        if (isinstance(val, Name) or
+           isinstance(val, AccountName) or
+           isinstance(val, PermissionName) or
+           isinstance(val, ActionName) or
+           isinstance(val, TableName) or
+           isinstance(val, ScopeName) ) :
+            val = self._write_name(val)
+            return val
+        elif(isinstance(val, str)) :
+            return self._write_str(val)
+        elif(isinstance(val, Byte) or
+             isinstance(val, bool)) :
+            #return self._write_number(val, '?')
+            return int_to_hex(val)
+        elif(isinstance(val, UInt16)) :
+            return self._write_number(val, 'H')
+        elif(isinstance(val,UInt32)) :
+            return self._write_number(val, 'I')
+        elif(isinstance(val,VarUInt)) :
+            # temp encoding
+            return self._write_varuint(val)
+        elif(isinstance(val, int) or
+             isinstance(val, long)) :
+            return self._write_number(val, 'l')
+        elif(isinstance(val, Authorization)) :
+            return val.encode()
+        elif(isinstance(val, Action)) :
+            return val.encode()
+        elif(isinstance(val, list)) :
+            buf = self._write_varuint(VarUInt(len(val))) 
+            for item in val :
+                e_item = self.encode(item)
+                buf = '{}{}'.format(buf, e_item)
+            return buf
+        else :
+            raise TypeError('Invalid type {} specified'.format(type(val)))
+        
