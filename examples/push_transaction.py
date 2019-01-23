ee8eca23e0fc561e24d10fa6c7df12a7163a5d57
Buddy Deck <deck@eosnewyork.io>
2019-01-23 11:41:01 -0500

    examples for using key files

--- examples/push_transaction.py
+++ examples/push_transaction.py
@@ -1,31 +1,28 @@
 import eospy.cleos
+import os
 
 ce = eospy.cleos.Cleos(url='http://api.pennstation.eosnewyork.io:7001')
 
-payload = [
-        {
-            'args': {
-                "from": "eosio",  # sender
-                "to": "bob123451234",  # receiver
-                "quantity": '1.0000 EOS',  # In EOS
-                "memo": "EOS to the moon",
-            },
-            "account": "eosio.token",
-            "name": "transfer",
-            "authorization": [{
-                "actor": "eosio",
-                "permission": "active",
-            }],
+arguments = {
+            "from": "eosio",  # sender
+            "to": "bob123451234",  # receiver
+            "quantity": '1.0000 EOS',  # In EOS
+            "memo": "EOS to the moon",
         }
-    ]
+payload = {
+        "account": "eosio.token",
+        "name": "transfer",
+        "authorization": [{
+            "actor": "eosio",
+            "permission": "active",
+        }],
+    }
 #Converting payload to binary
-data=ce.abi_json_to_bin(payload[0]['account'],payload[0]['name'],payload[0]['args'])
+data=ce.abi_json_to_bin(payload['account'],payload['name'],arguments)
 #Inserting payload binary form as "data" field in original payload
-payload[0]['data']=data['binargs']
-#Removing the arguments field
-payload[0].pop('args')
+payload['data']=data['binargs']
 #final transaction formed
-trx = {"actions": payload}
+trx = {"actions": [payload]}
 
 # use a string or EOSKey for push_transaction
 key = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
@@ -33,6 +30,33 @@ key = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
 # import eospy.keys
 # key = eospy.keys.EOSKey('5HuaTWKeGzZhqyzuzFAjjFjPnnnjdgcp562oBSS8Wv1qgDSkR2W')
 resp = ce.push_transaction(trx, key, broadcast=True)
+print('------------------------------------------------')
+print(resp)
+print('------------------------------------------------')
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+key_file = os.path.join(script_dir,'pennstation_eosio.key')
+
+# wait to send transaction
+import time
+print('Pushing a transaction after 10 seconds using key_file: {}'.format(key_file))
+time.sleep(10)
+
+arguments = {
+            "from": "eosio",  # sender
+            "to": "bob123451234",  # receiver
+            "quantity": '2.0000 EOS',  # In EOS
+            "memo": "EOS to the moon",
+        }
+
+#Converting payload to binary
+data=ce.abi_json_to_bin(payload['account'],payload['name'],arguments)
+#Inserting payload binary form as "data" field in original payload
+payload['data']=data['binargs']
+#final transaction formed
+trx = {"actions": [payload]}
+
+resp = ce.push_transaction(trx, key_file, broadcast=True)
 print('------------------------------------------------')
 print(resp)
 print('------------------------------------------------')
